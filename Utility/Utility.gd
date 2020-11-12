c146386577b98ba0924a0c692154c29a20f3cf41
Feniks <writetofeniks@gmail.com>
2020-11-12 10:46:45 +0000

    Added return_dictionary_key_from_value
    Given a dictionary and a value it returns corresponding key particularly useful for returning enum name from it's value.

--- Utility/Utility.gd
+++ Utility/Utility.gd
@@ -1,7 +1,7 @@
 extends Node
 # This node holds all reusable functions to be used accross multiple of nodes
 
-# text testing varibles
+# Text testing varibles
 var lorem_ipsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum non nulla tempor dui gravida ultrices a vel enim. Aliquam erat volutpat. Nulla malesuada purus ut justo placerat, et accumsan augue bibendum. Pellentesque mattis commodo efficitur. Maecenas auctor varius feugiat. Aenean eget lacus at nisi luctus maximus. Integer auctor placerat arcu, eu venenatis velit. In tincidunt ipsum id varius fringilla. Cras id porta libero, quis mattis neque. Nam eget nulla egestas nibh commodo porta sed vel ante. Donec viverra est lorem, vel fringilla massa rhoncus sed. Donec tristique nec mauris iaculis commodo"
 var lazy_dog = "The quick brown fox jumps over the lazy dog"
 
@@ -23,3 +23,9 @@ func do_percent_chance_check(chance:float) -> bool:
 		return false
 	else: 
 		return true
+
+
+func return_dictionary_key_from_value(my_dictonary, my_value):
+	# Returns key given a dictionary and it's value 
+		var key = my_dictonary.keys()[my_dictonary.values().find(my_value)]
+		return key
